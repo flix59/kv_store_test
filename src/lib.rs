757716bb4c2125181d7d1134f0efef7ea6756e7d
Felix Pieper <felix@aipieper.de>
2023-11-05 00:10:24 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod avl_tree;
+pub use avl_tree::AvlTree;
+
+// helper modules and blueprints for testing
+mod avl_test_wrapper;
+mod avl_tree_health;
+mod avl_test_wrapper_decimal;
+mod avl_test_wrapper_u16;
---
+++ tests/helper_kv_store.rs
@@ -0,0 +1,63 @@
+use std::mem;
+
+use lazy_static::lazy_static;
+use radix_engine::blueprints::package::PackageDefinition;
+use scrypto::prelude::*;
+use scrypto_testenv::*;
+use transaction::builder::ManifestBuilder;
+
+impl TestHelperExecution for TestHelper {
+    fn env(&mut self) -> &mut TestEnvironment {
+        &mut self.env
+    }
+}
+lazy_static! {
+    static ref PACKAGE: (Vec<u8>, PackageDefinition) = compile_package(this_package!());
+}
+
+pub struct TestHelper {
+    env: TestEnvironment,
+    tree_address: Option<ComponentAddress>,
+}
+
+impl TestHelper {
+    pub fn new() -> TestHelper {
+        let env = TestEnvironment::new(vec![("test", &PACKAGE)]);
+
+        TestHelper {
+            env,
+            tree_address: None,
+        }
+    }
+
+    pub fn instantiate(&mut self) -> &mut TestHelper {
+        let manifest_builder = mem::replace(&mut self.env.manifest_builder, ManifestBuilder::new());
+        self.env.manifest_builder = manifest_builder.call_function(
+            self.env.package_address("test"),
+            "KVStoreTest",
+            "instantiate",
+            manifest_args!()
+        );
+        self.env.new_instruction("instantiate", 1, 0);
+        self
+    }
+
+    pub fn instantiate_default(&mut self, verbose: bool) -> Receipt {
+        self.instantiate();
+        let receipt = self.execute_expect_success(verbose);
+        let pool_address: ComponentAddress = receipt.outputs("instantiate")[0];
+        self.tree_address = Some(pool_address);
+        receipt
+    }
+
+    pub fn insert(&mut self, key: Decimal, value: Decimal) -> &mut TestHelper {
+        let manifest_builder = mem::replace(&mut self.env.manifest_builder, ManifestBuilder::new());
+        self.env.manifest_builder = manifest_builder.call_method(
+            self.tree_address.unwrap(),
+            "insert",
+            manifest_args!(key, value)
+        );
+        self.env.new_instruction("insert", 1, 0);
+        self
+    }
+}
