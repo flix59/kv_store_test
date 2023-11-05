9e3456d89797ecc6b39010f75bc64f1a7ee72897
Felix Pieper <felix@aipieper.de>
2023-11-05 15:54:43 +0100

    test add exact limit

--- tests/test_limits.rs
+++ tests/test_limits.rs
@@ -7,7 +7,7 @@ mod kv_store_test {
     use super::*;
     use helper_kv_store::*;
 
-    #[test]
+    // #[test]
     fn test_insert_and_update() {
         let mut helper = TestHelper::new();
         helper.instantiate_default(false);
@@ -19,11 +19,49 @@ mod kv_store_test {
             helper.insert(key, Decimal::from(0));
             helper.execute_expect_success(true);
             helper.reset(key);
-            offset = offset* 2;
+            offset = offset * 2;
             key = key + offset;
             println!("inserts: {}", key);
         }
     }
+
+    #[test]
+    fn test_insert_success() {
+        let mut helper = TestHelper::new();
+        helper.instantiate_default(false);
+        let key = 497;
+        helper.insert(key, Decimal::from(key));
+        helper.execute_expect_success(true);
+        helper.reset(key);
+        println!("inserts: {}", key);
+    }
+    #[test]
+    fn test_insert_fail() {
+        let mut helper = TestHelper::new();
+        helper.instantiate_default(false);
+        let key = 498;
+        helper.insert(key, Decimal::from(key));
+        helper.execute_expect_success(true);
+        helper.reset(key);
+        println!("inserts: {}", key);
+    }
+
+    // #[test]
+    fn test_insert_with_startup() {
+        let mut helper = TestHelper::new();
+        helper.instantiate_default(false);
+        // Startup
+        helper.insert(1, Decimal::from(0));
+        helper.reset(1);
+        helper.execute_expect_success(true);
+        // Insert
+        let key = 497;
+        helper.insert(key, Decimal::from(key));
+        helper.execute_expect_success(true);
+        helper.reset(key);
+        println!("inserts: {}", key);
+    }
+
     // #[test]
     fn test_insert() {
         let mut helper = TestHelper::new();
@@ -34,7 +72,7 @@ mod kv_store_test {
             helper.insert(key, Decimal::from(key));
             helper.execute_expect_success(true);
             helper.reset(key);
-            offset = offset* 2;
+            offset = offset * 2;
             key = key + offset;
             println!("inserts: {}", key);
         }
@@ -42,4 +80,4 @@ mod kv_store_test {
 }
 
 // TODO how many read write in one operation
- // max size of KV store
+// max size of KV store
